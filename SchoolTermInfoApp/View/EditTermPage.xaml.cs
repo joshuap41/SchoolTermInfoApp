2d25b9ae6945efd49050f3f89f92c10486d0249a
joshua.j@outlook.com <j1490000>
2020-06-20 21:01:17 -0400

    finished term crud

--- SchoolTermInfoApp/View/EditTermPage.xaml.cs
+++ SchoolTermInfoApp/View/EditTermPage.xaml.cs
@@ -2,26 +2,59 @@
 using System.Collections.Generic;
 using SchoolTermInfoApp.Model;
 using Xamarin.Forms;
+using SQLite;
 
 namespace SchoolTermInfoApp.View
 {
     public partial class EditTermPage : ContentPage
     {
+        Term selectedTerm;
+
         public EditTermPage(Term selectedTerm)
         {
             InitializeComponent();
-        }
 
-        //void UpdateButton_Clicked(System.Object sender, System.EventArgs e)
-        //{
-        //}
+            this.selectedTerm = selectedTerm;
 
-        void DeleteButton_Clicked(System.Object sender, System.EventArgs e)
-        {
+            //populates the view
+            termName.Text = selectedTerm.TermName;
+            startDate.Date = selectedTerm.StartDate;
+            finishDate.Date = selectedTerm.FinishDate;
+
         }
 
         void UpdateButton_Clicked(System.Object sender, System.EventArgs e)
         {
+            selectedTerm.TermName = termName.Text;
+            selectedTerm.StartDate = startDate.Date;
+            selectedTerm.FinishDate = finishDate.Date;
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Term>();
+                int rows = conn.Update(selectedTerm);
+
+                if (rows > 0)
+                    DisplayAlert("Success", "Term successfully updated", "Ok");
+                else
+                    DisplayAlert("Failure", "Term failed to update", "Ok");
+            }
+
+        }
+
+        void DeleteButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Term>();
+                int rows = conn.Delete(selectedTerm);
+
+                if (rows > 0)
+                    DisplayAlert("Success", "Term successfully deleted", "Ok");
+                else
+                    DisplayAlert("Failure", "Term failed to delete", "Ok");
+            }
+
         }
     }
 }
